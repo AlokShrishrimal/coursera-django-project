b4ff7cb983a2bd2ea4097773e55b150289fa7ff2
AlokShrishrimal <alokshrishrimal2@gmail.com>
2026-09-09 21:17:23 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,135 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
-from django.views.generic import ListView, DetailView
-
-from .models import (
-    Course,
-    Lesson,
-    Instructor,
-    Learner,
-    Enrollment,
-    Question,
-    Choice,
-    Submission
-)
-
-
-class CourseListView(ListView):
-    model = Course
-    template_name = 'onlinecourse/course_list_bootstrap.html'
-    context_object_name = 'courses'
-
-
-class CourseDetailView(DetailView):
-    model = Course
-    template_name = 'onlinecourse/course_detail_bootstrap.html'
-    context_object_name = 'course'
-
-
-def registration_request(request):
-    if request.method == 'GET':
-        return render(
-            request,
-            'onlinecourse/registration_bootstrap.html'
-        )
-
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        first_name = request.POST.get('first_name', '')
-        last_name = request.POST.get('last_name', '')
-
-        if User.objects.filter(username=username).exists():
-            return render(
-                request,
-                'onlinecourse/registration_bootstrap.html',
-                {'error': 'Username already exists.'}
-            )
-
-        user = User.objects.create_user(
-            username=username,
-            password=password,
-            first_name=first_name,
-            last_name=last_name
-        )
-
-        login(request, user)
-
-        return HttpResponseRedirect(
-            reverse('onlinecourse:index')
-        )
-
-
-def login_request(request):
-    if request.method == 'GET':
-        return render(
-            request,
-            'onlinecourse/login_bootstrap.html'
-        )
-
-    username = request.POST['username']
-    password = request.POST['password']
-
-    user = authenticate(
-        request,
-        username=username,
-        password=password
-    )
-
-    if user is not None:
-        login(request, user)
-
-        return HttpResponseRedirect(
-            reverse('onlinecourse:index')
-        )
-
-    return render(
-        request,
-        'onlinecourse/login_bootstrap.html',
-        {'error': 'Invalid username or password.'}
-    )
-
-
-def logout_request(request):
-    logout(request)
-
-    return HttpResponseRedirect(
-        reverse('onlinecourse:index')
-    )
-
-
-def enroll(request, course_id):
-    if not request.user.is_authenticated:
-        return HttpResponseRedirect(
-            reverse('onlinecourse:login')
-        )
-
-    course = get_object_or_404(
-        Course,
-        pk=course_id
-    )
-
-    Enrollment.objects.get_or_create(
-        user=request.user,
-        course=course
-    )
-
-    return HttpResponseRedirect(
-        reverse(
-            'onlinecourse:course_details',
-            args=(course_id,)
-        )
-    )
-
-
-# ---------------------------------------------------------
-# EXAM SUBMISSION
-# ---------------------------------------------------------
-
 def submit(request, course_id):
+
     course = get_object_or_404(
         Course,
         pk=course_id
@@ -148,18 +18,16 @@ def submit(request, course_id):
     selected_choices = []
 
     for key in request.POST:
+
         if key.startswith('choice'):
+
             choice_id = request.POST[key]
 
-            try:
-                choice = Choice.objects.get(
-                    id=choice_id
-                )
+            choice = Choice.objects.get(
+                id=choice_id
+            )
 
-                selected_choices.append(choice)
-
-            except Choice.DoesNotExist:
-                pass
+            selected_choices.append(choice)
 
     submission.choices.set(
         selected_choices
@@ -176,15 +44,12 @@ def submit(request, course_id):
     )
 
 
-# ---------------------------------------------------------
-# EXAM RESULT
-# ---------------------------------------------------------
-
 def show_exam_result(
     request,
     course_id,
     submission_id
 ):
+
     course = get_object_or_404(
         Course,
         pk=course_id
@@ -197,9 +62,7 @@ def show_exam_result(
 
     score = 0
 
-    questions = course.question_set.all()
-
-    for question in questions:
+    for question in course.question_set.all():
 
         correct_choices = set(
             question.choice_set.filter(
@@ -219,8 +82,7 @@ def show_exam_result(
     context = {
         'course': course,
         'submission': submission,
-        'score': score,
-        'questions': questions
+        'score': score
     }
 
     return render(
